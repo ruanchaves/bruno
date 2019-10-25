fd277433766c116e0f79aaa49fc2242cacea9702
ruanchaves <ruanchaves@gmail.com>
2019-10-25 16:26:55 -0300

    tagger - incompleto

--- interpretador.lua
+++ interpretador.lua
@@ -1,5 +1,6 @@
 require 'utils'
 require 'reader'
+require 'tagger'
 
 --
 -- Pega o nome do arquivo passado como parâmetro (se houver)
@@ -16,9 +17,14 @@ if not file then
    os.exit(1)
 end
 
+verbose = true
 --
 -- Identifica as funções no arquivo
 --
-verbose = true
 function_list, function_name_to_index = func_reader(file, verbose)
 file:close()
+
+--
+-- Categoriza cada linha de cada função
+--
+function_list_tags = line_tagger(function_list, verbose)
--- tagger.lua
+++ tagger.lua
@@ -1,43 +1,63 @@
 require 'utils'
 
 function tag_match(line)
-    tokens = string_split(line)
-    
+    scores = {}
+
     patterns = {
-        1: {"function", "%(%)"},
-        2: {"function", "%(", "%)"},
-        3: {"var"},
-        4: {"var", "%["},
-        5: {"begin "},
-        6: {"end "},
-        7: {" %= "},
-        8: {" %= ", " %+ "," %- ", " %* ", " %/ "},
-        9: {"%(%)"},
-        10: {"%(", "%)"},
-        11: {"if ", "then "},
-        12: {"else"},
-        13: {"fi"}
+        [1]="function .+%(%)",
+        [2]="function .+%(.+%)",
+        [3]="var .+",
+        [4]="var %[.+",
+        [5]="^begin ",
+        [6]="^end ",
+        [7]="%w+ %= %w+",
+        [8]="%w+ %= %w+ %p %w+",
+        [9]="%w+%(%)",
+        [10]="%w+%(.+%)",
+        [11]="if %w+ %p %w+ then"
+        [12]="else",
+        [13]="fi"
     }
 
     labels = {
-        1: "header_1",
-        2: "header_2",
-        3: "var_1",
-        4: "var_2",
-        5: "begin",
-        6: "end",
-        7: "attr_1",
-        8: "attr_2",
-        9: "funcall_1",
-        10: "funcall_2",
-        11: "if_then",
-        12: "else",
-        13: "fi"
+        [1]="header_1",
+        [2]="header_2",
+        [3]="var_1",
+        [4]="var_2",
+        [5]="begin",
+        [6]="end",
+        [7]="attr_1",
+        [8]="attr_2",
+        [9]="funcall_1",
+        [10]="funcall_2",
+        [11]="if_then",
+        [12]="else",
+        [13]="fi"
     }
 
+    for key, value in ipairs(patterns) do
+        scores[key] = 0
+    end
+
+    for key, value in ipairs(patterns) do
+        if string.find(line, value) then
+            scores[key] = scores[key] + 1
+        end
+    end
+
+    chosen_label = nil
+    tmp_score = 0
+    for key, value in ipairs(scores) do
+        if value > tmp_score then
+            chosen_label = labels[key]
+        end
+    end
+
+    return chosen_label
+end
 
 function line_tagger(function_list, verbose)
-    verbose = verbose else false
+    verbose = verbose or false
     function_list_tags = {}
     function_block_tags = {}
     for key, func_lines in ipairs(function_list) do
